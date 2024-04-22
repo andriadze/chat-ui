84b9bafc4e5230fd739aa14e37ae95205ed394ab
Giorgi Andriadze <g.andriadze2@gmail.com>
2024-04-22 13:35:51 +0400

    fix: updated symbol

--- src/lib/constants/defaultGreeting.ts
+++ src/lib/constants/defaultGreeting.ts
@@ -1,1 +1,1 @@
-export const DEFAULT_GREETING = `Hey {user_name}! 🌟 I'm {char_name}. It's so nice to meet you 💕`;
+export const DEFAULT_GREETING = `Hey //user_name//! 🌟 I'm //char_name//. It's so nice to meet you 💕`;
